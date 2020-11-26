19d7d6dea46397dc0aeb421234101c3e80c71b69
EjaazLakhani1 <60117208+EjaazLakhani1@users.noreply.github.com>
2020-11-25 21:11:02 -0700

    Add files via upload

---
+++ project login/index.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
+});
+
+users = [];
+io.on('connection', function(socket) {
+   console.log('A user connected');
+   socket.on('setUsername', function(data) {
+      console.log(users);
+      
+      if(users.indexOf(data) > -1) {
+         socket.emit('userExists', data + ' username is taken! Try some other username.');
+      } else {
+         users.push(data);
+         console.log(users);
+         socket.emit('userSet', {username: data});
+      }
+   });
+   
+   
+});
+
+http.listen(3000, function() {
+   console.log('listening on localhost:3000');
+});
