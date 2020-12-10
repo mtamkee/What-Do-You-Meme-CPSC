2cbe5866880cba8c656eb0f65e1557a7288d7997
mtamkee <33907017+mtamkee@users.noreply.github.com>
2020-12-10 00:21:21 -0700

    rebase app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,23 +3,34 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import firebase from 'firebase/app';
 import { Observable } from 'rxjs';
- 
+
+
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent{
 
-  constructor(private router: Router) { 
-  }
-  
-  
+  constructor(private router: Router) { }
+
   title = "What Do You Meme? CPSC";
+
 
   appRouterActive() {
     console.log("App router outlet was activated.")
   }
-  
-}
 
+
+
+
+  ngOnInit(): void {
+
+  }
+
+
+}
